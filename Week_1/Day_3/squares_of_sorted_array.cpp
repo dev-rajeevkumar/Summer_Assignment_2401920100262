665fbd5e38eed8c70230306a983092aff32cbac3
dev-rajeevkumar <rajeevkumar21555@gmail.com>
2026-06-03 22:48:17 +0530

    Create squares_of_sorted_array.cpp

---
+++ Week_1/Day_3/squares_of_sorted_array.cpp
@@ -0,0 +1,11 @@
+// https://leetcode.com/problems/squares-of-a-sorted-array/submissions/2021494252/
+class Solution {
+public:
+    vector<int> sortedSquares(vector<int>& nums) {
+        for(int i=0;i<nums.size();i++){
+            nums[i]=nums[i]*nums[i];
+        }
+        sort(nums.begin(),nums.end());
+        return nums;
+    }
+};
