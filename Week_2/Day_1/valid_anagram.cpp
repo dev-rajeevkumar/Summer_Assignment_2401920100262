5a35ee3c6bfbf6bd4b1c1bbf0b796998db9dd603
dev-rajeevkumar <rajeevkumar21555@gmail.com>
2026-06-08 14:52:00 +0530

    Create valid_anagram.cpp

---
+++ Week_2/Day_1/valid_anagram.cpp
@@ -0,0 +1,19 @@
+// https://leetcode.com/problems/valid-anagram/submissions/2002451158/
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if(s.size()!=t.size())
+            return false;
+        for(int i=0;i<s.size();i++){
+            for(int j=0;j<t.size();j++){
+                if(s[i]==t[j]){
+                    t.erase(j, 1);
+                    break;
+                }
+            }
+        }
+        if(t.size()==0)
+            return true;
+        return false;
+    }
+};
